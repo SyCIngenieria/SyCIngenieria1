ad86186019636c94d1317bf3e488ed60124c9765
santiago <santiago@DESKTOP-Q952O0M>
2024-10-08 11:20:39 -0500

    Agregar archivos de proyecto.

---
+++ SyCIngenieria/Models/GestionContratos/Actas.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SyCIngenieria.Models.GestionContratos
+{
+    public class Actas
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("ODS")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int ODSId { get; set; }
+        public ODS? ODS { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        public string NombreActa { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(500, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [DataType(DataType.MultilineText)]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(500, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [DataType(DataType.Url)]
+        public string DocumentoAdjunto { get; set; }
+    }
+}
---
+++ SyCIngenieria/Models/GestionContratos/AmpliacionContrato.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace SyCIngenieria.Models.GestionContratos
+{
+    public class AmpliacionContrato
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Contrato")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int ContratoId { get; set; }
+        public Contrato? Contrato { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [DataType(DataType.Date)]
+        public DateTime FechaInicioAmpliacion { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [DataType(DataType.Date)]
+        public DateTime FechaFinAmpliacion { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [DataType(DataType.Currency)]
+        public decimal ValorAmpliacion { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "El campo {0} debe ser un valor no negativo")]
+        public int NumeroDiasAmpliacion { get; set; }
+    }
+}
