27a0bd954a5c66d81c33f4413c3e94d0b3716d49
intelisoft software SAS <S_y_C_Ingenieria@outlook.com>
2024-10-08 11:22:59 -0500

    Prueba 0 subir cambios

---
+++ SyCIngenieria/Models/GestionContratos/Contrato.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SyCIngenieria.Models.GestionContratos
+{
+    public class Contrato
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        public string NumeroContrato { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(500, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [DataType(DataType.MultilineText)]
+        public string Descripcion { get; set; }
+
+        [ForeignKey("Empresas")]
+        public int EmpresasId { get; set; }
+        public Empresas? Empresas { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        public string NombrePersonaContrato { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        public string NumeroDocumentoPersonaContrato { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailPersonaContrato { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [DataType(DataType.PhoneNumber)]
+        public string TelefonoPersonaContrato { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [DataType(DataType.Currency)]
+        public decimal ValorContrato { get; set; }
+
+        public int DuracionDiascontrato { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [DataType(DataType.Date)]
+        public DateTime Fecha_Inicio { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [DataType(DataType.Date)]
+        public DateTime Fecha_Fin { get; set; }
+
+        public ICollection<OrdenCambio>? OrdenesCambio { get; set; }
+    }
+}
